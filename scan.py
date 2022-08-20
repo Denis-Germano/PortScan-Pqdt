5fc416914ed3f436fd2e3a76f321563c26b1ceaf
ManinhuGuitar <denisgermano@live.com>
2022-08-20 14:54:19 -0300

    correções

--- scan.py
+++ scan.py
@@ -16,7 +16,6 @@ if len(sys.argv) == 4:
     print(f'\033[4;33;40mScan IP:\033[m {ip} nas portas {start_port} a {end_port} \n')
 
     try:
-        # Scan nas 65535 Ports TCP
         for port in range(int(start_port), int(end_port)):
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             socket.setdefaulttimeout(1)
@@ -24,8 +23,8 @@ if len(sys.argv) == 4:
             result = s.connect_ex((ip,port))
             if result == 0:
                 print(f"\033[7;30;42m[+]\033[m Porta {port} aberta")
-            else:
-                print(f'\033[7;30;41m[+]\033[m Porta {port} fechada') # Caso queira exibir as portas fechadas
+            #else:
+                #print(f'\033[7;30;41m[+]\033[m Porta {port} fechada') # Caso queira exibir as portas fechadas descomente o Else e o print
             s.close()
         
     except KeyboardInterrupt:
